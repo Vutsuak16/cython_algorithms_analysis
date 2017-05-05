a7cc3ba6c103ed08259df92cd64ea564e11a0257
Deolal <vutsuak96@gmail.com>
2017-05-05 15:07:16 +0530

    a prime number cythonic comparison

---
+++ prime_numbers/cythonic_prime.pyx
@@ -0,0 +1,23 @@
+#finds the 1000th prime 
+def prime(x):
+
+	number=2
+	primes=[]
+	while len(primes) != x:
+
+		factors=0
+		for i in range(1,number+1):
+			if number%i == 0:
+				factors+=1
+
+		if factors==2:
+			primes.append(number)
+		number+=1
+		if len(primes)==x:
+				print primes[-1]
+				return
+				
+
+
+
+# python -m timeit --number=4 "import cythonic_prime" "cythonic_prime.prime(100)"
